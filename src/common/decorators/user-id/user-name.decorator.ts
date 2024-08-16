15721de28e8200f4bb3dde5348671817e179caa4
a1185963755 <1185963755@qq.com>
2024-08-16 18:53:05 +0800

    chore:修改变量名

--- src/common/decorators/user-id/user-name.decorator.ts
+++ src/common/decorators/user-id/user-name.decorator.ts
@@ -3,6 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const UserName = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.params.name;
+    return request.params.username;
   },
 );
