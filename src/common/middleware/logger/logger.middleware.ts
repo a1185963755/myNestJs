a88bc10d4e9f3c4b2c65cd2735b06334580c6ca9
a1185963755 <1185963755@qq.com>
2024-08-20 11:59:52 +0800

    feat(casbin,authorization): 增加了RBAC权限控制

--- src/common/middleware/logger/logger.middleware.ts
+++ src/common/middleware/logger/logger.middleware.ts
@@ -3,7 +3,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    console.log('调用了cat接口');
+    console.log('调用了users接口');
 
     next();
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,22 +11,22 @@ import {
 import { UsersService } from './users.service';
 import { createUserSchema, CreateUserDto } from './dto/create-user.dto';
 import { HelloWorldInterceptor } from 'src/common/interceptors/hello-world/hello-world.interceptor';
-import { AuthGuard } from 'src/common/guards/auth/auth.guard';
-import { Auth } from 'src/common/decorators/auth/auth.decorator';
+
 import { UserName } from 'src/common/decorators/user-id/user-name.decorator';
+import { RoleGuard } from 'src/common/guards/role/role.guard';
 
 @Controller('users')
-@UseGuards(AuthGuard)
 @UseInterceptors(HelloWorldInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @UseGuards(RoleGuard)
   @Get()
   @Header('Cache-Control', 'no-cache, no-store, must-revalidate')
   async findAll() {
     return this.usersService.findAll();
   }
 
-  @Auth('admin')
+  @UseGuards(RoleGuard)
   @Post()
   async create(@Body() createCatDto: CreateUserDto) {
     const validation = createUserSchema.safeParse(createCatDto);
@@ -38,6 +38,7 @@ export class UsersController {
     return this.usersService.register(validation.data);
   }
 
+  @UseGuards(RoleGuard)
   @Get(':username')
   async findOne(@UserName() username: string) {
     return this.usersService.findOne(username);
